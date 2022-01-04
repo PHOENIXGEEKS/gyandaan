4bad1ea13e642ec3942e96c511144635c9a4da16
Subin <subinsk284@gmail.com>
2022-01-04 15:55:15 +0530

    features updated

--- src/components/LandingPage/Features.js
+++ src/components/LandingPage/Features.js
@@ -1,8 +1,40 @@
 import React from 'react'
+// import * as icons from '../../images/feature_icons'
+import importAll from '../../utilities/importAllImages'
 
 function Features() {
+    const icons = []
+    const images = importAll(require.context('../../images/feature_icons', false, /\.(png|jpe?g|svg)$/));
+    Object
+        .entries(images)
+        .forEach(([key, value]) => {
+            icons.push(value)
+        })
+    const texts = [
+        'Doubt solving',
+        'Towards Excellence',
+        'Best Mentors',
+        'Live Lectures',
+        'Tutors'
+    ]
     return (
-        <div className="flex justify-between w-full h-80 bg-primary-red">
+        <div className="flex items-center justify-between w-full h-80 p-5 bg-primary-red">
+            {
+                [
+                    icons.map((icon, key) => {
+                        return (
+                            <div className="flex flex-col space-y-2 items-center">
+
+                                <img className='invert h-32' src={icon} alt='icon' />
+                                {
+                                    <p className='invert font-bold text-2xl'>{texts[key]}</p>
+                                }
+                            </div>
+                        )
+                    }
+                    ),
+                ]
+            }
 
         </div>
     )
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -12,14 +12,14 @@ function Navbar() {
           Gyaan<span className="text-primary-red underline">Daan</span>
         </div>
 
-        <div id="auth" className="space-x-12">
+        <div id="auth" className="flex justify-between items-center space-x-8">
           <Link id="login_btn" to="/auth">
-            <div className="border-2 border-primary-red bg-primary-red px-8 py-2 rounded-full text-white">
+            <div className="border-2 border-primary-red bg-primary-red px-6 py-2 rounded-full text-white">
               Login
             </div>
           </Link>
           <Link id="signup_btn" to="/auth">
-            <div className="border-2 border-primary-red px-8 py-2 rounded-full text-primary-red">
+            <div className="border-2 border-primary-red px-6 py-2 rounded-full text-primary-red">
               Join for Free
             </div>
           </Link>
